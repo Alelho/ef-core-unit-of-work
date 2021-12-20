789fc910fb3fd201dfafc4e340cc89d4f1e6175e
Alexadre Carvalho <ac.alexandre.carvalho@outlook.com>
2021-12-19 23:13:20 -0300

    add test

--- tests/EFCoreDataAccess.Tests/UnitOfWorkTests.cs
+++ tests/EFCoreDataAccess.Tests/UnitOfWorkTests.cs
@@ -2,9 +2,12 @@
 using EFCoreDataAccess.Data.Repositories;
 using EFCoreDataAccess.Interfaces;
 using EFCoreDataAccess.Models;
+using EFCoreDataAccess.Models.Interface;
 using EFCoreDataAccess.Repository;
 using EFCoreDataAccess.Tests.Infra;
+using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 using Xunit;
 
 namespace EFCoreDataAccess.Tests
@@ -43,6 +46,21 @@ namespace EFCoreDataAccess.Tests
 
             // Assert
             Assert.IsAssignableFrom<AddressRepository>(genericRepository);
+        }
+
+        [Fact]
+        public void GetRepository_ShouldReturnAnException_GivenAnInterface()
+        {
+            // Arrange
+            var uow = _databaseFixture.ServiceProvider.GetService<IUnitOfWork<EmployeeDbContext>>();
+
+            // Act
+            Action act = () => uow.GetRepository<IAddressRepository>();
+
+            // Assert
+            act.Should()
+                .Throw<InvalidOperationException>()
+                .WithMessage("The type TRepository should be a class");
         }
     }
 }
