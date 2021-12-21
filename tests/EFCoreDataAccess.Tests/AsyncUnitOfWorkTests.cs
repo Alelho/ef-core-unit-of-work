14afaebeb7ebf395bd887f0d841ec283fb287f1b
Alexadre Carvalho <ac.alexandre.carvalho@outlook.com>
2021-12-21 00:29:54 -0300

    add more tests

---
+++ tests/EFCoreDataAccess.Tests/AsyncUnitOfWorkTests.cs
@@ -0,0 +1,38 @@
+﻿using EFCoreDataAccess.Data;
+using EFCoreDataAccess.Interfaces;
+using EFCoreDataAccess.Tests.Infra;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EFCoreDataAccess.Tests
+{
+    [CollectionDefinition("Database collection")]
+    public class AsyncUnitOfWorkTests : IClassFixture<DatabaseFixture>
+    {
+        private readonly DatabaseFixture _databaseFixture;
+
+        public AsyncUnitOfWorkTests(DatabaseFixture databaseFixture)
+        {
+            _databaseFixture = databaseFixture;
+        }
+
+        [Fact]
+        public async Task BeginTransactionAsync_Throw_TryCreateMultiplesTransactions()
+        {
+            // Arrange
+            var uow = _databaseFixture.ServiceProvider.GetService<IUnitOfWork<EmployeeDbContext>>();
+
+            await uow.BeginTransactionAsync();
+
+            // Act
+            Func<Task> act = () => uow.BeginTransactionAsync();
+
+            // Assert
+            await act.Should().ThrowAsync<InvalidOperationException>()
+                .WithMessage("There is already an active transaction");
+        }
+    }
+}
