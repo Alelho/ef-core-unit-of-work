7fb495c58076bf5dff1e3a805b30a92233b76c06
Alexadre Carvalho <ac.alexandre.carvalho@outlook.com>
2021-12-27 22:22:09 -0300

    create more routes for controllers sample

--- src/EFCoreDataAccess.API/Controllers/EmployeesController.cs
+++ src/EFCoreDataAccess.API/Controllers/EmployeesController.cs
@@ -1,8 +1,11 @@
-﻿using EFCoreDataAccess.Data;
+﻿using EFCoreDataAccess.API.Requests;
+using EFCoreDataAccess.Data;
 using EFCoreDataAccess.Interfaces;
 using EFCoreDataAccess.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 
 namespace EFCoreDataAccess.API.Controllers
 {
@@ -17,7 +20,25 @@ namespace EFCoreDataAccess.API.Controllers
             _unitOfWork = unitOfWork;
         }
 
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Employee), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+        public IActionResult GetEmployee(long id)
+        {
+            var employeeRepository = _unitOfWork.GetGenericRepository<Employee>();
+
+            var employee = employeeRepository.SingleOrDefault(e => e.Id == id);
+
+            if (employee == null) return NotFound();
+
+            return Json(employee);
+        }
+
         [HttpGet("company/{companyId}")]
+        [ProducesResponseType(typeof(IEnumerable<Employee>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
         public IActionResult GetEmployeesByCompanyId(long companyId)
         {
             var employeeRepository = _unitOfWork.GetGenericRepository<Employee>();
@@ -27,6 +48,23 @@ namespace EFCoreDataAccess.API.Controllers
             if (!employees.Any()) return NotFound();
 
             return Json(employees);
+        }
+
+        [HttpPost("")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+        public IActionResult CreateEmployee([FromBody] CreateEmployeeRequest request)
+        {
+            var employeeRepository = _unitOfWork.GetGenericRepository<Employee>();
+
+            var employee = new Employee(request.Name, request.Code, request.Position, request.BirthDate);
+            employee.SetCompany(request.CompanyId);
+
+           employeeRepository.Add(employee);
+
+            _unitOfWork.SaveChanges();
+
+            return Ok();
         }
     }
 }
---
+++ src/EFCoreDataAccess.API/Requests/CreateEmployeeRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EFCoreDataAccess.API.Requests
+{
+    public class CreateEmployeeRequest
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Position { get; set; }
+        public DateTime BirthDate { get; set; }
+        public long CompanyId { get; set; }
+    }
+}
