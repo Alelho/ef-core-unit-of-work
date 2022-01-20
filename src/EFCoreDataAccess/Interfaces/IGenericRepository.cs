df3eecd2d299494c980e4a0fb81c7487ef7e41bc
Alexadre Carvalho <ac.alexandre.carvalho@outlook.com>
2022-01-19 23:16:37 -0300

    remove duplicated interface

--- src/EFCoreDataAccess/Interfaces/IGenericRepository.cs
+++ src/EFCoreDataAccess/Interfaces/IGenericRepository.cs
@@ -5,6 +5,6 @@ namespace EFCoreDataAccess.Interfaces
 	public interface IGenericRepository : IDisposable
     { }
 
-    public interface IGenericRepository<T> : IGenericRepository, ISyncGenericRepository<T>, IAsyncGenericRepository<T> where T : class
+    public interface IGenericRepository<T> : ISyncGenericRepository<T>, IAsyncGenericRepository<T> where T : class
     { }
 }
