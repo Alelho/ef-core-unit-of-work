1515026bf6f5e432077efece9838087d9b7a6a90
Alexadre Carvalho <ac.alexandre.carvalho@outlook.com>
2022-07-07 20:55:12 -0300

    fix GetPropertyInfo method to support converted types

--- tests/EFCoreDataAccess.Tests/ExpressionFuncExtensionstests.cs
+++ tests/EFCoreDataAccess.Tests/ExpressionFuncExtensionstests.cs
@@ -1,13 +1,14 @@
 ﻿using EFCoreDataAccess.Models;
 using EFCoreUnitOfWork.Extensions;
 using FluentAssertions;
+using FluentAssertions.Execution;
 using System;
 using System.Linq.Expressions;
 using Xunit;
 
 namespace EFCoreDataAccess.Tests
 {
-	public class ExpressionFuncExtensionstests
+	public class ExpressionFuncExtensionsTests
 	{
         [Fact]
         public void GetPropertyInfo_ShouldThrowArgumentException_GivenMethodExpression()
@@ -29,17 +30,33 @@ namespace EFCoreDataAccess.Tests
         public void GetPropertyInfo_ShouldReturnThePropertyInfo_GivenPropertyExpression()
         {
             // Arrange
-            var company = new Company(name: "Disney");
+            var employee = new Employee(name: "Disney", code: "123", position: "Software Developer", birthDate: new DateTime(2000, 02, 10));
+            var employeeEarnings = new EmployeeEarnings(Models.Enums.PaymentPeriodType.Monthly);
 
-            var expectedResult = company.GetType().GetProperty("Name");
+            var expectedNameResult = employee.GetType().GetProperty("Name");
+            var expectedBirthDateResult = employee.GetType().GetProperty("BirthDate");
+            var expectedCompanyIdResult = employee.GetType().GetProperty("CompanyId");
+            var expectedPaymentPeriodResult = employeeEarnings.GetType().GetProperty("PaymentPeriodType");
 
-            Expression<Func<Company, object>> expression = (c) => c.Name;
+            Expression<Func<Employee, object>> nameExpression = (e) => e.Name;
+            Expression<Func<Employee, object>> birthDateExpression = (e) => e.BirthDate;
+            Expression<Func<Employee, object>> companyIdExpression = (e) => e.CompanyId;
+            Expression<Func<EmployeeEarnings, object>> paymentPeriodExpression = (e) => e.PaymentPeriodType;
 
             // Act
-            var result = expression.GetPropertyInfo();
+            var nameExpressionResult = nameExpression.GetPropertyInfo();
+            var birthDateExprssionResult = birthDateExpression.GetPropertyInfo();
+            var companyIdExprssionResult = companyIdExpression.GetPropertyInfo();
+            var paymentPeriodExprssionResult = paymentPeriodExpression.GetPropertyInfo();
 
             // Assert
-            result.Should().Equals(expectedResult);
+            using (new AssertionScope())
+            {
+                nameExpressionResult.Should().Equals(expectedNameResult);
+                birthDateExprssionResult.Should().Equals(expectedBirthDateResult);
+                companyIdExprssionResult.Should().Equals(expectedCompanyIdResult);
+                paymentPeriodExprssionResult.Should().Equals(expectedPaymentPeriodResult);
+            }
         }
     }
 }
